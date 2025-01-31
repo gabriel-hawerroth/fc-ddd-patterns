f207933090ffdc00d2305cf03ad5d2700dbfed24
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2025-01-31 13:23:46 -0300

    Add Address, Customer, Order, and OrderItem classes with validation and main application logic

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,35 +1,31 @@
-class Customer {
+import Address from "./address";
+
+export default class Customer {
   _id: string;
   _name: string;
-  _address: string;
+  _address: Address;
+  _active: boolean = false;
 
-  constructor(id: string, name: string, address: string) {
+  constructor(id: string, name: string, address: Address) {
     this._id = id;
     this._name = name;
     this._address = address;
   }
 
-  get id() {
-    return this._id;
+  validate() {
+    if (this._id.trim().length === 0) {
+      throw new Error("ID is required");
+    }
+    if (this._name.trim().length === 0) {
+      throw new Error("Name is required");
+    }
   }
 
-  get name() {
-    return this._name;
+  activate() {
+    this._active = true;
   }
 
-  get address() {
-    return this._address;
-  }
-
-  set id(id: string) {
-    this._id = id;
-  }
-
-  set name(name: string) {
-    this._name = name;
-  }
-
-  set address(address: string) {
-    this._address = address;
+  deactivate() {
+    this._active = false;
   }
 }
