31bb916087fa70702ec476fde8be66e8938a038e
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2025-02-04 13:33:23 -0300

    Enhance Order and OrderItem classes with validation for quantity and price, update test cases, and modify package.json scripts

--- src/entity/order_item.ts
+++ src/entity/order_item.ts
@@ -1,22 +1,56 @@
 export default class OrderItem {
   private _id: string;
+  private _productdId: string;
   private _name: string;
   private _price: number;
   private _quantity: number;
 
-  constructor(id: string, name: string, price: number, quantity: number) {
+  constructor(
+    id: string,
+    name: string,
+    price: number,
+    productId: string,
+    quantity: number
+  ) {
     this._id = id;
     this._name = name;
     this._price = price;
+    this._productdId = productId;
     this._quantity = quantity;
+    this.validate();
   }
 
   get price(): number {
-    return this._price;
+    return this._price * this._quantity;
+  }
+
+  get quantity(): number {
+    return this._quantity;
   }
 
   set price(value: number) {
     this._price = value;
+    this.validate();
+  }
+
+  set quantity(value: number) {
+    this._quantity = value;
+    this.validate();
+  }
+
+  validate() {
+    if (!this._id?.trim()) {
+      throw new Error("ID is required");
+    }
+    if (!this._name?.trim()) {
+      throw new Error("Name is required");
+    }
+    if (this._price < 0) {
+      throw new Error("Price must be greater than zero");
+    }
+    if (this._quantity <= 0) {
+      throw new Error("Quantity must be greater than zero");
+    }
   }
 
   orderItemTotal(): number {
--- test/entity/order.spec.ts
+++ test/entity/order.spec.ts
@@ -2,9 +2,9 @@ import Order from "../../src/entity/order";
 import OrderItem from "../../src/entity/order_item";
 
 describe("Order unit testes", () => {
-  const item = new OrderItem("1", "Product 1", 10, 1);
-  const item2 = new OrderItem("2", "Product 2", 20, 1);
-  const item3 = new OrderItem("3", "Product 3", 30, 1);
+  const item = new OrderItem("1", "Product 1", 10, "p1", 1);
+  const item2 = new OrderItem("2", "Product 2", 20, "p2", 1);
+  const item3 = new OrderItem("3", "Product 3", 30, "p3", 1);
 
   it("should throw an error when ID is not provided", () => {
     expect(() => new Order("", "1", [item])).toThrow("ID is required");
@@ -26,5 +26,11 @@ describe("Order unit testes", () => {
     const order = new Order("1", "1", [item, item2, item3]);
 
     expect(order.total).toBe(60);
+  });
+
+  it("should check if quantity is greater than zero", () => {
+    expect(() => (item.quantity = 0)).toThrow(
+      "Quantity must be greater than zero"
+    );
   });
 });
