94088957aed73684e817947b7f7f5d50a8694341
gabriel-hawerroth <gabrielhawerroth04@gmail.com>
2025-03-17 22:05:34 -0300

    Add Order and OrderItem models with associations and initial repository tests

---
+++ test/infrastructure/order.repository.spec.ts
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize-typescript";
+import CustomerModel from "../../src/infrastructure/db/sequelize/model/customer.model";
+
+describe("Order repository tests", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([CustomerModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+});
